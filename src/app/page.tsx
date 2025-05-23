50f01034a919eed7d36305228568d3aedb90206b
aewnix <okkatte@gmail.com>
2025-05-23 11:57:25 +0200

    basic style fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">  
-      <h1 className="text-3xl font-bold text-red-500">Hello Tailwind!</h1>
-    </div>
-  );
+    <main className="p-6">
+    </main>
+  )
 }
